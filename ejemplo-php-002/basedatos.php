61a5038ad4c2f1180608c96634aeedbddb4d6656
René Elizalde <reroes799@gmail.com>
2026-06-10 08:53:31 -0500

    ejemplo

--- ejemplo-php-002/basedatos.php
+++
@@ -1,9 +0,0 @@
-<?php
-  // datos para enlace la base de datos
-  $servidor = "localhost";
-  $usuario = "su-usuario";
-  $password = "su-clave";
-  $base_datos = "mobiliaria";
-  $conectaBD = new mysqli($servidor, $usuario, $password, $base_datos);
-  $conectaBD->set_charset("utf8");
-?>
